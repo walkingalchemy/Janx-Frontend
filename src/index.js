cbc6cd07055a4a751c3e0791d65dc5fd1cd4eba7
KellyStamps <kellydsample@gmail.com>
2018-02-09 14:49:00 -0500

    ready for websocket

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//web socket stuff, NEED TO NPM INSTALL REACT ACTIONCABLE PROVIDER
+// import {ApplicationCableProvider } from 'react-actioncable-provider'
+
+ReactDOM.render(
+  //NEED TO NPM INSTALL FIRST
+  // <ApplicationCableProvider >
+      <App />,
+  // </ApplicationCableProvider>,
+  document.getElementById('root'));
 registerServiceWorker();
