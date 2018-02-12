596e8105cc220aa61f8bb3164b8fc65eb50ddc7c
KellyStamps <kellydsample@gmail.com>
2018-02-12 09:27:35 -0500

    organized components folder and started on post

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -14,6 +14,18 @@ class ChatContainer extends React.Component {
       transcript: [...this.state.transcript, [event.target.line.value, new Date().toLocaleTimeString()]]
     })
     event.target.line.value = ''
+    this.postHelper()
+  }
+
+  postHelper = () => {
+    fetch(`http://localhost:3001/chat_sessions`, {
+      method: 'POST',
+      body: JSON.stringify(this.state.transcript),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(res=>res.json()).then(data=>console.log(data))
   }
 
   render (){
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 //web socket stuff, NEED TO NPM INSTALL REACT ACTIONCABLE PROVIDER
